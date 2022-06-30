ae52c99c890bc9427a5f0adb25f93a79dfcd28bc
geo <tp53.geo@gmail.com>
2022-07-01 00:45:19 +0200

    ajout de fonctions pour valider le formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const radioButtons = document.querySelectorAll(".checkbox-input");
+const userAgreementCheckbox = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -26,5 +28,24 @@ function launchModal() {
 
 // close modal form
 function closeModal() {
+  checkUserAgreementIsSelected();
   modalbg.style.display = "none";
+}
+
+// check if at leat 1 location radio button is selected
+function checkLocationIsSelected() {
+  for (let radio of radioButtons) {
+    if (radio.checked && radio.value != "on") {
+      return true;
+    }
+  }
+  return false;
+}
+
+// check if user agreement checkbox is selected
+function checkUserAgreementIsSelected() {
+  if (userAgreementCheckbox.checked) {
+    return true;
+  }
+  return false;
 }
