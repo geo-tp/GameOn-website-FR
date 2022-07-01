c24f56032700ed49a96497cc080e94f25cca71ec
geo <tp53.geo@gmail.com>
2022-07-01 02:49:13 +0200

    implementation du processus de validation et integration des messages d'erreur dans le html

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -6,16 +6,27 @@ const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const radioButtons = document.querySelectorAll(".checkbox-input");
 const userAgreementCheckbox = document.getElementById("checkbox1");
+const allInputs = document.getElementsByTagName("input");
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const birthRegex =
   /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
+
+inputEventOnChange();
+
+function inputEventOnChange() {
+  for (let input of allInputs) {
+    input.addEventListener("input", () => hideError(input));
+  }
+}
 
 // check if lastname length is greater than 2
 function checkLastNameIsValid() {
   if (lastname.value.length >= 2) {
     return true;
   }
+
+  displayError(lastname);
 
   return false;
 }
@@ -26,6 +37,8 @@ function checkFirstNameIsValid() {
     return true;
   }
 
+  displayError(firstname);
+
   return false;
 }
 
@@ -35,23 +48,29 @@ function checkEmailIsValid() {
     return true;
   }
 
+  displayError(email);
+
   return false;
 }
 
 // check if birthdate format is valid
-function checkBirthDateIsProvided() {
+function checkBirthDateIsValid() {
   if (birthdate.value.match(birthRegex)) {
     return true;
   }
+
+  displayError(birthdate);
 
   return false;
 }
 
 // check if tournament participation's number is provided
-function checkParcipationNumberIsProvided() {
+function checkParcipationNumberIsValid() {
   if (typeof quantity === "number") {
     return true;
   }
+
+  displayError(quantity);
 
   return false;
 }
@@ -63,6 +82,9 @@ function checkLocationIsSelected() {
       return true;
     }
   }
+
+  displayError(radioButtons);
+
   return false;
 }
 
@@ -71,5 +93,35 @@ function checkUserAgreementIsSelected() {
   if (userAgreementCheckbox.checked) {
     return true;
   }
+
+  displayError(userAgreementCheckbox);
+
   return false;
+}
+
+function displayError(element) {
+  parent = element.parentNode;
+  parent.setAttribute("data-error-visible", true);
+}
+
+function hideError(element) {
+  console.log(element);
+  parent = element.parentNode;
+  parent.setAttribute("data-error-visible", false);
+}
+
+function validate(e) {
+  e.preventDefault();
+
+  if (
+    checkFirstNameIsValid() &&
+    checkLastNameIsValid() &&
+    checkEmailIsValid() &&
+    checkBirthDateIsValid() &&
+    checkLocationIsSelected() &&
+    checkParcipationNumberIsValid() &&
+    checkUserAgreementIsSelected()
+  ) {
+    // DO SUCCESS
+  }
 }
