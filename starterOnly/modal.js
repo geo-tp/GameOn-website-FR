4ef35dbce147567813eeff6b193b293782950eb3
geo <tp53.geo@gmail.com>
2022-07-01 05:12:40 +0200

    implementation mise a jour modal quand form est validé et envoyé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,15 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBody = document.querySelectorAll(".modal-body");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal evant
-closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
@@ -26,6 +27,24 @@ function launchModal() {
 
 // close modal form
 function closeModal() {
-  checkUserAgreementIsSelected();
   modalbg.style.display = "none";
+}
+
+// update modal when form is validated
+function updateToConfirmationModal() {
+  modalBody[0].style.visibility = "hidden";
+  let confirmationText = document.createElement("p");
+  let confirmationClose = document.createElement("button");
+  let confirmationContainer = document.createElement("div");
+  let parent = modalBody[0].parentNode;
+
+  confirmationText.textContent = "Merci pour votre inscription";
+  confirmationText.classList.add("modal-confirmation-text");
+  confirmationClose.textContent = "Fermer";
+  confirmationClose.classList.add("modal-confirmation-close", "btn-submit");
+  confirmationClose.addEventListener("click", closeModal);
+  confirmationContainer.classList.add("modal-confirmation-container");
+  confirmationContainer.appendChild(confirmationText);
+  confirmationContainer.appendChild(confirmationClose);
+  parent.appendChild(confirmationContainer);
 }
